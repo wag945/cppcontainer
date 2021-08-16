0608bb6e399a746b28c752e89b633c2b52f099bd
Bill Ganley <wag945@gmail.com>
2021-08-16 08:10:12 -0400

    Add MyClass

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include "MyClass.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    	cout << "Hello World from container" << endl;
+    cout << "Hello World from container" << endl;
+	MyClass myClass;
+	myClass.setName("Test");
+	cout << "Name: " << myClass.getName() << endl;
 	return(0);
 }
